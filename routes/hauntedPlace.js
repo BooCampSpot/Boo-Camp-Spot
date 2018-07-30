dc9fa6d51726ac3f1408c274cfdfe6faa98bd115
vedantm8 <vedantm8@gmail.com>
2018-07-30 14:03:55 -0400

    hantedPlaceRoute done

---
+++ routes/hauntedPlace.js
@@ -0,0 +1,19 @@
+var dataBase = require('../models');
+
+module.exports = function (req, res) {
+    app.get('/api/HauntedPlaces', function(HauntedPlaces){
+        dataBase.HauntedPlaces.findAll({}).then(function(dataBase_HauntedPlaces){
+            res.json(dataBase_HauntedPlaces);
+        })
+    });
+    app.post('/api/HauntedPlaces', function(HauntedPlaces){
+        dataBase.HauntedPlaces.create(req.body).then(function(dataBase_HauntedPlaces){
+            res.json(dataBase_HauntedPlaces);
+        })
+    });
+    app.delete('/api/HauntedPlaces/:id', function(dataBase_HauntedPlaces){
+        dataBase_HauntedPlaces.destroy(req.body).then(function(dataBase_HauntedPlaces){
+            res.json(dataBase_HauntedPlaces);
+        });
+    });
+}
