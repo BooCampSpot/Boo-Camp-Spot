6c2d64daa0c30eed9b1038ee107eedaf951a86c5
vedantm8 <vedantm8@gmail.com>
2018-07-30 13:20:03 -0400

    types

--- models/type.js
+++ models/type.js
@@ -4,7 +4,7 @@ module.exports = function(sequelize, DataTypes) {
     });
   
     Type.associate = function(models) {
-      Type.hasMany(models.Post, {
+      Type.hasMany(models.HauntedPlace, {
       });
     };
   
