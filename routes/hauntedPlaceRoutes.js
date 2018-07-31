a9a1bc64cc1ffbc82ce3c3179c216573d770c092
vedantm8 <vedantm8@gmail.com>
2018-07-31 10:37:30 -0400

    fixingRoutes

--- routes/hauntedPlaceRoutes.js
+++ routes/hauntedPlaceRoutes.js
@@ -2,21 +2,25 @@ var dataBase = require('../models');
 
 module.exports = function (req, res) {
     //This gets all of the Haunted Places. 
-    app.get('/api/HauntedPlaces', function(HauntedPlaces){
+    app.get('/api/HauntedPlaces', function(req, res){
         dataBase.HauntedPlaces.findAll({}).then(function(dataBase_HauntedPlaces){
             res.json(dataBase_HauntedPlaces);
         })
     });
     //This allows the user to post new Haunted Places.
-    app.post('/api/HauntedPlaces', function(HauntedPlaces){
+    app.post('/api/HauntedPlaces', function(req, res){
         dataBase.HauntedPlaces.create(req.body).then(function(dataBase_HauntedPlaces){
             res.json(dataBase_HauntedPlaces);
         })
     });
     //This allows the user to delete one of the haunted places. 
-    app.delete('/api/HauntedPlaces/:id', function(dataBase_HauntedPlaces){
-        dataBase_HauntedPlaces.destroy(req.body).then(function(dataBase_HauntedPlaces){
+    app.delete('/api/HauntedPlaces/:id', function(req, res){
+        dataBase.HauntedPlaces.destroy({ where: { id: req.params.id } }).then(function(dataBase_HauntedPlaces){
             res.json(dataBase_HauntedPlaces);
         });
     });
+    //Render a 404 error 
+    app.get('*', function(req, res){
+        res.render('404 Error')
+    })
 }
