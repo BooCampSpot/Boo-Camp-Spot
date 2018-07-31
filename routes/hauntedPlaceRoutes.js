0a747d072fbeffa168f61ee1cbcb28de2e3e0acc
vedantm8 <vedantm8@gmail.com>
2018-07-31 10:27:48 -0400

    Double Checking

--- routes/hauntedPlaceRoutes.js
+++ routes/hauntedPlaceRoutes.js
@@ -1,17 +1,19 @@
 var dataBase = require('../models');
 
 module.exports = function (req, res) {
-    //This gets all of the 
+    //This gets all of the Haunted Places. 
     app.get('/api/HauntedPlaces', function(HauntedPlaces){
         dataBase.HauntedPlaces.findAll({}).then(function(dataBase_HauntedPlaces){
             res.json(dataBase_HauntedPlaces);
         })
     });
+    //This allows the user to post new Haunted Places.
     app.post('/api/HauntedPlaces', function(HauntedPlaces){
         dataBase.HauntedPlaces.create(req.body).then(function(dataBase_HauntedPlaces){
             res.json(dataBase_HauntedPlaces);
         })
     });
+    //This allows the user to delete one of the haunted places. 
     app.delete('/api/HauntedPlaces/:id', function(dataBase_HauntedPlaces){
         dataBase_HauntedPlaces.destroy(req.body).then(function(dataBase_HauntedPlaces){
             res.json(dataBase_HauntedPlaces);
