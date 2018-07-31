bd1c3bcf4bdb3f3b4ec55cbdde968717f00cdf0d
Julia Snyder <julesanne493@gmail.com>
2018-07-31 13:56:37 -0400

    JavaScript stuff

--- public/js/index.js
+++ public/js/index.js
@@ -3,25 +3,122 @@
 var $sightingButton = $("#sighting-button");
 var $placeButton = $("#place-button");
 var $userLogin = $("#user-login");
-var $reviewLocation = $("#review-location");
+var $reviewLocation = $("#reviewLocation");
 var $oneStar = $("#oneStar");
 var $twoStar = $("#twoStar");
 var $threeStar = $("#threeStar");
 var $fourStar = $("#fourStar");
 var $fiveStar = $("#fiveStar");
 var $reviewSubmit = $("#reviewSubmit");
+var reviewStar = 0
 
+var review = {
+  reviewLocation: $("#review-location").val(),
+  reviewStar: reviewStar,
+  reviewText: $("#reviewText").val(),
+}
+
+
+// Toggle review stars and store review value
 $oneStar.on("click", function() {
   if ($(this).attr("data-selected") === "false") {
     $(this).addClass("starClicked");
     $(this).attr("data-selected", "true");
-    review = "1";
-  }
-  else if ($(this).attr("data-selected") === "true"){
-    
-  }
+    reviewStar = 1;
+  } else {
+    $(this).removeClass("starClicked");
+    $(this).attr("data-selected", "false");
+  };
+  return reviewStar;
 });
 
+
+$twoStar.on("click", function() {
+  if ($(this).attr("data-selected") === "false") {
+    $(this).addClass("starClicked");
+    $oneStar.addClass("starClicked");
+    $(this).attr("data-selected", "true");
+    reviewStar = "2";
+  } else {
+    $(this).removeClass("starClicked");
+    $oneStar.removeClass("starClicked");
+    $(this).attr("data-selected", "false");
+    $oneStar.attr("data-selected", "false");
+  }
+  return reviewStar;
+});
+
+$threeStar.on("click", function() {
+  if ($(this).attr("data-selected") === "false") {
+    $(this).addClass("starClicked");
+    $oneStar.addClass("starClicked");
+    $twoStar.addClass("starClicked");
+    $(this).attr("data-selected", "true");
+    reviewStar = "3";
+  } else {
+    $(this).removeClass("starClicked");
+    $oneStar.removeClass("starClicked");
+    $twoStar.removeClass("starClicked");
+    $(this).attr("data-selected", "false");
+    $oneStar.attr("data-selected", "false");
+    $twoStar.attr("data-selected", "false");
+  }
+  return reviewStar;
+});
+
+$fourStar.on("click", function() {
+  if ($(this).attr("data-selected") === "false") {
+    $(this).addClass("starClicked");
+    $oneStar.addClass("starClicked");
+    $twoStar.addClass("starClicked");
+    $threeStar.addClass("starClicked");
+    $(this).attr("data-selected", "true");
+    reviewStar = "4";
+  } else {
+    $(this).removeClass("starClicked");
+    $oneStar.removeClass("starClicked");
+    $twoStar.removeClass("starClicked");
+    $threeStar.removeClass("starClicked");
+    $(this).attr("data-selected", "false");
+    $oneStar.attr("data-selected", "false");
+    $twoStar.attr("data-selected", "false");
+    $threeStar.attr("data-selected", "false");
+  }
+  return reviewStar;
+});
+
+$fiveStar.on("click", function() {
+  if ($(this).attr("data-selected") === "false") {
+    $(this).addClass("starClicked");
+    $oneStar.addClass("starClicked");
+    $twoStar.addClass("starClicked");
+    $threeStar.addClass("starClicked");
+    $fourStar.addClass("starClicked");
+    $(this).attr("data-selected", "true");
+    reviewStar = "5";
+  } else {
+    $(this).removeClass("starClicked");
+    $oneStar.removeClass("starClicked");
+    $twoStar.removeClass("starClicked");
+    $threeStar.removeClass("starClicked");
+    $fourStar.removeClass("starClicked");
+    $(this).attr("data-selected", "false");
+    $oneStar.attr("data-selected", "false");
+    $twoStar.attr("data-selected", "false");
+    $threeStar.attr("data-selected", "false");
+    $fourStar.attr("data-selected", "false");
+  }
+  return reviewStar;
+});
+
+
+$reviewSubmit.on("click", function (){
+  event.preventDefault();
+  console.log(review.reviewLocation);
+  console.log(review.reviewStar);
+  console.log(reviewText);
+})
+  ;
 /*
 var reviewFormSubmit = function(event) {
   event.preventDefault();
